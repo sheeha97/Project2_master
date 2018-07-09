6ea1ea80435a7a53f8e2c5a633044925b1899a33
sheeha97 <sheeha97@users.noreply.github.com>
2018-07-10 01:13:20 +0900

    gallery done bam

---
+++ app/src/main/java/com/example/q/project2_master/Fragments/PregameFragment.java
@@ -0,0 +1,42 @@
+package com.example.q.project2_master.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.q.project2_master.R;
+
+public class PregameFragment extends Fragment {
+    private View v;
+
+    public PregameFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        v = inflater.inflate(R.layout.fragment_pre_game, container, false);
+        Button pregameButton = v.findViewById(R.id.pregame_button);
+        pregameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.output, fragment);
+                transaction.commit();
+            }
+        });
+
+
+        return v;
+    }
+
+
+}
