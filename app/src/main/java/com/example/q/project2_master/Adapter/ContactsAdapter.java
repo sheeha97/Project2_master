3e574a40302fb019d8c1bc0b759dd99eab4e9077
sheeha97 <sheeha97@users.noreply.github.com>
2018-07-09 15:08:16 +0900

    dialog oh baby

--- app/src/main/java/com/example/q/project2_master/Adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/q/project2_master/Adapter/ContactsAdapter.java
@@ -1,15 +1,22 @@
 package com.example.q.project2_master.Adapter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.q.project2_master.Activities.AppStartActivity;
@@ -72,6 +79,11 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
 
                 /*SharedPreferences sf = mContext.getSharedPreferences("user_name_saver", 0);
                 userName = sf.getString("user_name_preference", "");*/  //TODO: maybe MainActivity.userName can be replaced by this??
+
+                //Open dialog
+                openDialog();
+
+                //Create jsonString
                 ContactsModel person = new ContactsModel(MainActivity.userName, contact_name.getText().toString(), contact_number.getText().toString());
                 String jsonString = JsonUtils.toJSon(person);
 
@@ -101,6 +113,67 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
         public ShareContactServerSS(String urlTail, String stringData, Activity context, int method) {
             super(urlTail, stringData, context, method);
         }
+    }
+
+    //will see if i wanna create a new class for this
+    public void openDialog() {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+
+        // Set Custom Title
+        TextView title = new TextView(mContext);
+        // Title Properties
+        title.setText("Custom Dialog Box");
+        title.setPadding(10, 10, 10, 10);   // Set Position
+        title.setGravity(Gravity.CENTER);
+        title.setTextColor(Color.BLACK);
+        title.setTextSize(20);
+        alertDialog.setCustomTitle(title);
+
+        // Set Message
+        TextView msg = new TextView(mContext);
+        // Message Properties
+        msg.setText("I am a Custom Dialog Box. \n Please Customize me.");
+        msg.setGravity(Gravity.CENTER_HORIZONTAL);
+        msg.setTextColor(Color.BLACK);
+        alertDialog.setView(msg);
+
+        // Set Button
+        // you can more buttons
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL,"OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // Gotta do something fancy here @.@
+
+
+                //fill out the blank
+
+
+
+            }
+        });
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE,"CANCEL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // do nothing
+            }
+        });
+
+        new Dialog(mContext);
+        alertDialog.show();
+
+        // Set Properties for OK Button
+        final Button okBT = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+        LinearLayout.LayoutParams neutralBtnLP = (LinearLayout.LayoutParams) okBT.getLayoutParams();
+        neutralBtnLP.gravity = Gravity.FILL_HORIZONTAL;
+        okBT.setPadding(50, 10, 10, 10);   // Set Position
+        okBT.setTextColor(Color.BLUE);
+        okBT.setLayoutParams(neutralBtnLP);
+
+        final Button cancelBT = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+        LinearLayout.LayoutParams negBtnLP = (LinearLayout.LayoutParams) okBT.getLayoutParams();
+        negBtnLP.gravity = Gravity.FILL_HORIZONTAL;
+        cancelBT.setTextColor(Color.RED);
+        cancelBT.setLayoutParams(negBtnLP);
     }
 
 }
